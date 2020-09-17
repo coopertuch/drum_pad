cdb9de295e9055e7ca5e3700f1313d029f6d9733
Cooper Tuch <coopertuch@gmail.com>
2020-09-17 14:31:23 -0700

    function buttons

--- src/components/bass.js
+++ src/components/bass.js
@@ -1,16 +1,22 @@
 import React from "react";
+import "./bass.css";
 
-let Bass = () => (
-  <div>
-    <button>One</button>
-    <button>Two</button>
-    <button>Three</button>
-    <button>Four</button>
-    <button>Five</button>
-    <button>Six</button>
-    <button>Seven</button>
-    <button>Eight</button>
-  </div>
-);
+let Bass = () => {
+  function playSound() {
+    alert("Test");
+  }
+  return (
+    <div>
+      <button onClick={playSound}>One</button>
+      <button>Two</button>
+      <button>Three</button>
+      <button>Four</button>
+      <button>Five</button>
+      <button>Six</button>
+      <button>Seven</button>
+      <button>Eight</button>
+    </div>
+  );
+};
 
 export default Bass;
