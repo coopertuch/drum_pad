ab856bc12c4e77c44e4f013aadae1c9a30a2bbf9
Cooper Tuch <coopertuch@gmail.com>
2020-09-17 14:18:09 -0700

    setting up components

---
+++ src/components/bass.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+let Bass = () => (
+  <div>
+    <button>One</button>
+    <button>Two</button>
+    <button>Three</button>
+    <button>Four</button>
+    <button>Five</button>
+    <button>Six</button>
+    <button>Seven</button>
+    <button>Eight</button>
+  </div>
+);
+
+export default Bass;
